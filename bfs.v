72e089c31db6088cc6c32a6d2824dbe5ebec866b
smazcw3 <arshadmail99_zaidi@yahoo.co.in>
2016-10-08 18:30:40 -0400

    Committing representation of node and graph on 10/08/2016 -1830EST

--- bfs.v
+++ bfs.v
@@ -1,3 +1,11 @@
 (*Authors: Syed Mohammed Arshad Zaidi, Anuj Sharma*)
 
 Require Import Coq.Lists.List.
+
+(*This is representation of single node of a graph*)
+Inductive node :=
+ Node : nat -> node.
+
+(*This is representation of graph*)
+Definition graph :=
+ list (node * list node).
