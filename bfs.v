b9227e2bf145a7dde269be4304e59e6cec89ce72
smazcw3 <arshadmail99_zaidi@yahoo.co.in>
2016-10-21 20:46:22 -0400

    Committing functions search_node and is_node_present 10/21/2016 20:47 EST

--- bfs.v
+++ bfs.v
@@ -18,3 +18,45 @@ Definition are_nodes_equal :
 (* Wrapper function on are_nodes_equal to verify equality of nodes. *)
 Definition nodes_equal a b :=
   if are_nodes_equal a b then true else false.
+
+ (* Raw function to search a given node in a given graph. *)
+ Definition search_node {A:Type} (ps:list(node*A)) (x:node) :=
+ match find (fun p => nodes_equal x (fst p)) ps with
+ | Some p => Some (snd p)
+ | None => None
+ end.
+ 
+ (* Wrapper function on search_node for searching node in a given graph. *)
+ Definition is_node_present {A:Type} (ps:list(node*A)) (default:A) (x:node) :=
+ match search_node ps x with
+ | None => default
+ | Some y => y
+ end.
+ 
+ (** Since pairs are used quite a bit, it is nice to be able to
+ write them with the standard mathematical notation [(x,y)] instead
+ of [pair x y].  We can tell Coq to allow this with a [Notation]
+ declaration. *)
+ 
+ Notation "( x , y )" := (pair x y).
+ (** As with pairs, it is more convenient to write lists in
+ familiar programming notation.  The following declarations
+ allow us to use [::] as an infix [cons] operator and square
+ brackets as an "outfix" notation for constructing lists. *)
+ 
+ Notation "x :: l" := (cons x l)
+ (at level 60, right associativity).
+ Notation "[ ]" := nil.
+ Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+ 
+ Definition customNode := Node 1.
+ Definition noCustomNode := Node 404.
+ Definition customGraph := [(Node 1, [Node 2; Node 3]);(Node 2, [Node 3;Node 4])].
+ 
+ Check customGraph.
+ 
+ Compute (search_node customGraph (Node 2)).
+ 
+ Compute (nodes_equal (Node 2) (Node 4)).
+ 
+ Compute (is_node_present customGraph [] customNode).
