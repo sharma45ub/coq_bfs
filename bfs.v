a634bf21d8cf0a4380fdc9b9cb8501732a58765d
Anuj Sharma <anujsharma@Anujs-MacBook-Pro.local>
2016-10-21 20:42:50 -0400

    commiting functions are_nodes_equal nodes_equal 21stOct2016 20:43

--- bfs.v
+++ bfs.v
@@ -9,3 +9,12 @@ Inductive node :=
 (* This is representation of graph *)
 Definition graph :=
  list (node * list node).
+
+(* Raw function to check equality of nodes. *)
+Definition are_nodes_equal :
+  forall (x y:node),
+    {x = y} + {x <> y}. repeat (decide equality). Defined.
+
+(* Wrapper function on are_nodes_equal to verify equality of nodes. *)
+Definition nodes_equal a b :=
+  if are_nodes_equal a b then true else false.
